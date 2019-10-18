d1ce8b0ae85dcba0e6556797cc5bf7c80bc0222d
unknown <email.sethu@gmail.com>
2019-10-18 13:42:34 +0530

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to BODYGLOVE
+                    Welcome to Body Glove
                 </div>
 
                
