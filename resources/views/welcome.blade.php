e6adc8a08a6025990b297c74636f0e04551c12e1
unknown <email.sethu@gmail.com>
2019-10-18 14:55:36 +0530

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ url('admin') }}">Admin Login</a>
+                        <a href="{{ url('/admin') }}">Admin Login</a>
 
                         
                     @endauth
