b087733129984b9e21d2fe14ed41ad5032d94603
unknown <email.sethu@gmail.com>
2019-10-19 18:06:26 +0530

    first

--- database/migrations/create_stockpoint.php
+++ database/migrations/create_stockpoint.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCountTable extends Migration
+class CreateStockPointTable extends Migration
 {
     /**
      * Run the migrations.
