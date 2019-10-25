065fe074ff5b716a8a1c4f2244b81623c426e94f
satheeshdomainindia <satheesh@dreamstudio.in>
2019-10-25 22:23:05 +0530

    app'

--- app/Http/Controllers/Admin/Settings/ColourController.php
+++ app/Http/Controllers/Admin/Settings/ColourController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 class ColourController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $colours= Colour::orderBy('name','asc')->paginate(5);
@@ -18,24 +13,11 @@ class ColourController extends Controller
      
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('colours.create');
          
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    
     public function store(Request $request)
     {        
         $request->validate([
@@ -48,62 +30,24 @@ class ColourController extends Controller
      
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Colours  $colours
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Colours $colours)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Colours  $colours
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Colour $colour)
     {
-        //return  $colour;
-      //  return  view('colours', $colour);
-      //echo 'test';
-       //$colours=Colours::where('id',$colours);
        return  view('colours.edit',compact('colour'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Colours  $colours
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request)
     {
-        //return  $request;
-        Colour::where('id', $request->id)
-        ->update(['name'=>$request->name ]);       
+        Colour::where('id', $request->id)->update(['name'=>$request->name ]);       
         $msg =['message' => 'Colour Updated successfully!'];
-         return  redirect('admin/colour')->with($msg);
+        return  redirect('admin/colour')->with($msg);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Colours  $colours
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Colour $colour)
     {
        // Colour::where('id', $colour->id)->delete();
        //  $colour->delete();
-
         $msg =['message' => 'Unable to Delete!',
         'type' => 'warning'];
-
         return  redirect('admin/colour')->with($msg);
     }
 }
