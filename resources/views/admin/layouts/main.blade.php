c4b1c838725576082905441321bc1db25c652dc6
unknown <email.sethu@gmail.com>
2019-12-16 14:36:55 +0530

    first

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -41,7 +41,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>B</b>GV</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Body</b>Glove</span>
+      <span class="logo-lg"><b>Body</b>Glove.info</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
