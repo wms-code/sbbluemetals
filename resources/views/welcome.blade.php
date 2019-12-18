206fa0fb39e3ca9b1449e8d8fe1555a404967dbd
unknown <email.sethu@gmail.com>
2019-12-18 12:45:04 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Body Glove
+                    Welcome to SB Blue Metals
                 </div>
 
                
