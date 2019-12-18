1b9bd1e8a2856c2d506307d0642e0e2fec440693
unknown <email.sethu@gmail.com>
2019-12-18 13:47:52 +0530

    111

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to SB Blue  ggg
+                    Welcome to SB Blue  111
                 </div>
 
                
