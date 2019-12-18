a9bac27669af811f3324f86afaa59fb0e1e11ce9
unknown <email.sethu@gmail.com>
2019-12-18 13:23:51 +0530

    test
    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to SB Blue 
+                    Welcome to SB Blue  dfgdfgdfgdfg
                 </div>
 
                
